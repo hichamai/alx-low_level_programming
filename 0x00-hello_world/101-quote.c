65f9c47c169e0e565b7a2f86dde7cd1bdb8c347f
HichamAi <achrafaqsbi16@gmail.com>
2023-03-17 16:32:46 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h>
 
 /**
- * main -Entry point
- * Return: Always 1
+ * main - print the sentence
+ * Return: return the value 1
  */
 
 int main(void)
 {
-	printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
-	return (1);
+    printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+    return (1);
 }
